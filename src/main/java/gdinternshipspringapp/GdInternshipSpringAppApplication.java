da534ca137acd6c217c0dc8565d8a0cc54452836
Aleksei <apukhovoi@griddynamics.com>
2022-02-04 19:06:01 +0300

    add spring app

---
+++ src/main/java/gdinternshipspringapp/GdInternshipSpringAppApplication.java
@@ -0,0 +1,13 @@
+package gdinternshipspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdInternshipSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdInternshipSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gdinternshipspringapp/GdInternshipSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package gdinternshipspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdInternshipSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
